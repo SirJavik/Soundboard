a6d589ec4c02f3bf57edaf19bef028ef6ebb13d8
Javik <b.schneider@goinput.de>
2021-11-09 09:11:25 +0100

    Removed conda

--- soundboard.py
+++ soundboard.py
@@ -1,9 +1,13 @@
 import os
 import soundboardconstants
+import logging
+import threading
+import sounddevice
+import time
 
 from tkinter import *
 from tkinter import ttk
-from playsound import playsound
+from tkinter import messagebox
 from resource_path import *
 from filehandler import *
 
@@ -22,20 +26,73 @@ for i in range(3):
     soundboardRoot.columnconfigure(i, weight=1)
     soundboardRoot.rowconfigure(i, weight=1)
 
-def playaudio():
-    play(AudioSegment.from_wav("sounds/sinalco.wav"))
+playAudio = False
+runAudioThread = True
 
+"""
+    Soundboard Statusbar
+"""
+statusbartext = StringVar()
+statusbartext.set(soundboardconstants.STATUSBARDEFAULT)
+statusbar = Label(soundboardRoot, textvariable=statusbartext, bd=1, relief=SUNKEN, anchor=W)
+statusbar.grid(row=3, column=0, sticky='ew', columnspan=3)
+
+
+def playaudio():
+    global playAudio
+
+    playAudio = True
+
+
+def playaudiothread(name):
+    name = "Audio"
+
+    global playAudio, statusbartext
+    logging.info("Thread %s: starting", name)
+
+    while runAudioThread:
+        sound = AudioSegment.from_file("sounds/sinalco.wav", format="wav")
+
+        if playAudio:
+            logging.info("Thread %s: playing audio", name)
+            statusbartext.set("Playing audio...")
+            play(sound)
+            logging.info("Thread %s: sleeping", name)
+            statusbartext.set(soundboardconstants.STATUSBARDEFAULT)
+
+        playAudio = False
+
+    logging.info("Thread %s: finishing", name)
+
+
+def doshutdown():
+    global runAudioThread
+    statusbartext.set("Exiting...")
+
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        runAudioThread = False
+        soundboardRoot.destroy()
+
+    statusbartext.set(soundboardconstants.STATUSBARDEFAULT)
 
 
 """
     Soundboard Menubar
 """
 menubar = Menu(soundboardRoot)
+
 filemenu = Menu(menubar)
-filemenu.add_command(label="Load configuration",    command=onopen)
-filemenu.add_command(label="Save configuration",    command=onsave)
-filemenu.add_command(label="Exit",                  command=soundboardRoot.destroy)
+filemenu.add_command(label="Load configuration", command=onopen)
+filemenu.add_command(label="Save configuration", command=onsave)
+filemenu.add_command(label="Exit", command=soundboardRoot.destroy)
+
+devicemenu = Menu(menubar)
+for element in sounddevice.query_devices():
+    if element["max_input_channels"] == 0 & element["hostapi"] == 0:
+        devicemenu.add_radiobutton(label=element["name"])
+
 menubar.add_cascade(label="Application", menu=filemenu)
+menubar.add_cascade(label="Device", menu=devicemenu)
 soundboardRoot.config(menu=menubar)
 
 """
@@ -46,17 +103,25 @@ rowCount = 0
 btnNumber = 1
 while rowCount != 3:
     for i in range(3):
-        ttk.Button(soundboardRoot, text=f"Button {btnNumber}", command=playaudio).grid(column=i, row=rowCount, sticky="nesw")
+        ttk.Button(soundboardRoot, text=f"Button {btnNumber}", command=playaudio).grid(column=i, row=rowCount,
+                                                                                       sticky="nesw")
         btnNumber += 1
     rowCount += 1
 
-
-
-"""
-    Soundboard Statusbar
-"""
-statusbar = Label(soundboardRoot, text="To Boldly Go Where No Man Has Gone Before.", bd=1, relief=SUNKEN, anchor=W)
-statusbar.grid(row=3, column=0, sticky='ew', columnspan=3)
-
 if __name__ == '__main__':
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    logging.info("Main    : before creating thread")
+    audioThread = threading.Thread(target=playaudiothread, args=(1,))
+    logging.info("Main    : before running thread")
+    audioThread.start()
+    logging.info("Main    : wait for the thread to finish")
+
+    print("Found audio devices:")
+    print(sounddevice.query_devices())
+
+    soundboardRoot.protocol("WM_DELETE_WINDOW", doshutdown)
     soundboardRoot.mainloop()
+    logging.info("Main    : all done")
--- soundboardconstants.py
+++ soundboardconstants.py
@@ -2,3 +2,4 @@ from resource_path import *
 
 BASEPATH  = resource_path(".")
 SOUNDPATH = resource_path(BASEPATH + "/sounds")
+STATUSBARDEFAULT = "To Boldly Go Where No Man Has Gone Before."
